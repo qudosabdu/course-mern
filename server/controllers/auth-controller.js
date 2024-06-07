b10d16cbef49e340113ec686860691196c65f06e
abdul qudoos <abdulqudoospk737@gmail.com>
2024-06-07 23:27:14 +0500

    created user model

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/userModel");
+
 const home = async (req, res) => {
   try {
     res.send("Home Page");
@@ -7,21 +9,42 @@ const home = async (req, res) => {
 };
 
 const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Please provide all fields" });
+  }
+
   try {
-    res.status(200).send("Login Page");
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    if (user.password !== password) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({ message: "Login successful" });
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
 const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: "Please provide all fields" });
+  }
+
   try {
-    res.status(200).json(
-        {
-            message: "Register Page",
-            body: req.body,
-        }
-    );
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    await User.create({ username, email, password });
+    
+    res.status(201).json({ message: "User created" });
   } catch (error) {
     res.status(500).send(error.message);
   }
---
+++ server/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please provide a username"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
